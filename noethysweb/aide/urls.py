f995a9c624b95a71b78e581c5288c1c45936145f
Noethys <Noethys@users.noreply.github.com>
2021-04-27 18:58:30 +0200

    Ajout de l'app Aide

---
+++ noethysweb/aide/urls.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2019-2021 Ivan LUCAS.
+#  Noethysweb, application de gestion multi-activités.
+#  Distribué sous licence GNU GPL.
+
+from django.urls import include, path
+from core.views import toc
+
+
+urlpatterns = [
+
+    # Table des matières
+    path('aide/', toc.Toc.as_view(menu_code="aide_toc"), name='aide_toc'),
+
+
+]
