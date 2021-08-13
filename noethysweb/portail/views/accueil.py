ccab8e3d8ee1402f4fc37eda30a05452c1bf1594
Noethys <Noethys@users.noreply.github.com>
2021-08-13 23:15:02 +0200

    Finalisation de la page d'accueil du portail

--- noethysweb/portail/views/accueil.py
+++ noethysweb/portail/views/accueil.py
@@ -3,10 +3,13 @@
 #  Noethysweb, application de gestion multi-activités.
 #  Distribué sous licence GNU GPL.
 
+import datetime
 from django.views.generic import TemplateView
+from django.db.models import Q
 from portail.views.base import CustomView
+from portail.utils import utils_approbations
 from individus.utils import utils_pieces_manquantes
-from core.models import PortailMessage
+from core.models import PortailMessage, Article
 
 
 class Accueil(CustomView, TemplateView):
@@ -16,6 +19,18 @@ class Accueil(CustomView, TemplateView):
     def get_context_data(self, **kwargs):
         context = super(Accueil, self).get_context_data(**kwargs)
         context['page_titre'] = "Accueil"
-        context['pieces_manquantes'] = utils_pieces_manquantes.Get_pieces_manquantes(famille=self.request.user.famille, only_invalides=True)
-        context['liste_messages_non_lus'] = PortailMessage.objects.filter(famille=self.request.user.famille, utilisateur__isnull=False, date_lecture__isnull=True)
+
+        # Pièces manquantes
+        context['nbre_pieces_manquantes'] = len(utils_pieces_manquantes.Get_pieces_manquantes(famille=self.request.user.famille, only_invalides=True))
+
+        # Messages non lus
+        context['nbre_messages_non_lus'] = len(PortailMessage.objects.filter(famille=self.request.user.famille, utilisateur__isnull=False, date_lecture__isnull=True))
+
+        # Approbations
+        approbations_requises = utils_approbations.Get_approbations_requises(famille=self.request.user.famille)
+        context['nbre_approbations_requises'] = approbations_requises["nbre_total"]
+
+        # Articles
+        conditions = Q(statut="publie") & Q(date_debut__lte=datetime.datetime.now()) & (Q(date_fin__isnull=True) | Q(date_fin__gte=datetime.datetime.now()))
+        context['articles'] = Article.objects.filter(conditions).order_by("-date_debut")
         return context
