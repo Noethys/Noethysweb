3490427d17792a6fa174bd2fa09fc7adc28a2f2b
Noethys <Noethys@users.noreply.github.com>
2021-08-16 15:28:58 +0200

    Ajout du public aux articles

--- noethysweb/portail/views/accueil.py
+++ noethysweb/portail/views/accueil.py
@@ -9,7 +9,7 @@ from django.db.models import Q
 from portail.views.base import CustomView
 from portail.utils import utils_approbations
 from individus.utils import utils_pieces_manquantes
-from core.models import PortailMessage, Article
+from core.models import PortailMessage, Article, Inscription
 
 
 class Accueil(CustomView, TemplateView):
@@ -30,7 +30,12 @@ class Accueil(CustomView, TemplateView):
         approbations_requises = utils_approbations.Get_approbations_requises(famille=self.request.user.famille)
         context['nbre_approbations_requises'] = approbations_requises["nbre_total"]
 
+        # Récupération des activités de la famille
+        inscriptions = Inscription.objects.select_related("activite").filter(famille=self.request.user.famille)
+        activites = list({inscription.activite: True for inscription in inscriptions}.keys())
+
         # Articles
         conditions = Q(statut="publie") & Q(date_debut__lte=datetime.datetime.now()) & (Q(date_fin__isnull=True) | Q(date_fin__gte=datetime.datetime.now()))
-        context['articles'] = Article.objects.filter(conditions).order_by("-date_debut")
+        conditions &= (Q(public="toutes") | (Q(public="inscrits") & Q(activites__in=activites)))
+        context['articles'] = Article.objects.select_related("image_article").filter(conditions).distinct().order_by("-date_debut")
         return context
