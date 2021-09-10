88eb66c52ef45aeba1dfb47f28fb99da92f96c22
Noethys <Noethys@users.noreply.github.com>
2021-09-10 11:11:19 +0200

    Affichage des conditions d'utilisation dans la page de login

--- noethysweb/portail/views/change_password.py
+++ noethysweb/portail/views/change_password.py
@@ -15,6 +15,17 @@ class MyPasswordChangeView(ClassCommuneLogin, auth_views.PasswordChangeView):
     success_url = reverse_lazy('password_change_done')
     template_name = "portail/password_change_form.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(MyPasswordChangeView, self).get_context_data(**kwargs)
+
+        # Fusion du texte des conditions légales avec les valeurs organisateur
+        texte_conditions = context['parametres_portail'].get("mentions_conditions_generales", "")
+        for nom_champ in ("nom", "rue", "cp", "ville"):
+            texte_conditions = texte_conditions.replace("{ORGANISATEUR_%s}" % nom_champ.upper(), getattr(context['organisateur'], nom_champ))
+        context['texte_conditions'] = texte_conditions
+
+        return context
+
     def form_valid(self, form):
         form.save()
         utilisateur = form.user
