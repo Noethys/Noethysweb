41540b68e35094a5d66020a5d0357184be0825c1
Noethys <Noethys@users.noreply.github.com>
2022-01-15 09:45:16 +0100

    Ajout de la colonne Tarif à la liste des prestations

--- noethysweb/facturation/views/liste_prestations.py
+++ noethysweb/facturation/views/liste_prestations.py
@@ -25,7 +25,7 @@ class Liste(Page, crud.Liste):
     model = Prestation
 
     def get_queryset(self):
-        return Prestation.objects.select_related('activite', 'famille', 'individu').filter(self.Get_filtres("Q"))
+        return Prestation.objects.select_related('activite', 'famille', 'individu', 'tarif').filter(self.Get_filtres("Q"))
 
     def get_context_data(self, **kwargs):
         context = super(Liste, self).get_context_data(**kwargs)
@@ -36,18 +36,21 @@ class Liste(Page, crud.Liste):
         return context
 
     class datatable_class(MyDatatable):
-        filtres = ["ipresent:individu", "fpresent:famille", "idprestation", "date", "label", "montant", "activite__nom", "famille__nom", "individu__nom", "individu__prenom"]
+        filtres = ["ipresent:individu", "fpresent:famille", "idprestation", "date", "label", "montant", "activite__nom", "famille__nom", "individu__nom", "individu__prenom",
+                   "tarif__idtarif", "tarif__date_debut"]
 
         check = columns.CheckBoxSelectColumn(label="")
         activite = columns.TextColumn("Activité", sources=['activite__nom'])
         individu = columns.CompoundColumn("Individu", sources=['individu__nom', 'individu__prenom'])
         famille = columns.TextColumn("Famille", sources=['famille__nom'])
+        tarif = columns.TextColumn("ID Tarif", sources=['tarif__idtarif'])
+        tarif_date_debut = columns.TextColumn("Tarif Début", sources=['tarif__date_debut'], processor=helpers.format_date("%d/%m/%Y"))
 
         class Meta:
             structure_template = MyDatatable.structure_template
-            columns = ['check', "idprestation", "date", "label", "montant", "activite", "famille", "individu"]
+            columns = ['check', "idprestation", "date", "label", "montant", "activite", "famille", "individu", "tarif", "tarif_date_debut"]
             processors = {
-                'date': helpers.format_date('%d/%m/%Y'),
+                "date": helpers.format_date("%d/%m/%Y"),
             }
             ordering = ["date"]
 
