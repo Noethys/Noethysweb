a948426d81d77b222a5a1185b270468908f32021
Noethys <Noethys@users.noreply.github.com>
2022-02-13 21:04:03 +0100

    Affiche les auteurs des mails et sms

--- noethysweb/outils/views/emails.py
+++ noethysweb/outils/views/emails.py
@@ -3,12 +3,11 @@
 #  Noethysweb, application de gestion multi-activités.
 #  Distribué sous licence GNU GPL.
 
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
+from django.db.models import Q, Count
 from core.views.mydatatableview import MyDatatable, columns, helpers
 from core.views import crud
 from core.models import Mail
-from django.db.models import Q, Count
-
 
 
 class Page(crud.Page):
@@ -29,7 +28,7 @@ class Liste(Page, crud.Liste):
     model = Mail
 
     def get_queryset(self):
-        return Mail.objects.filter(self.Get_filtres("Q")).annotate(nbre_destinataires=Count("destinataires"))
+        return Mail.objects.select_related("utilisateur").filter(self.Get_filtres("Q")).annotate(nbre_destinataires=Count("destinataires"))
 
     def get_context_data(self, **kwargs):
         context = super(Liste, self).get_context_data(**kwargs)
@@ -40,23 +39,29 @@ class Liste(Page, crud.Liste):
         return context
 
     class datatable_class(MyDatatable):
-        filtres = ['idmail', 'objet']
-
+        filtres = ['idmail', 'date_creation', 'objet']
         check = columns.CheckBoxSelectColumn(label="")
         actions = columns.TextColumn("Actions", sources=None, processor='Get_actions_standard')
         nbre_destinataires = columns.TextColumn("Destinataires", sources=['nbre_destinataires'])
+        utilisateur = columns.TextColumn("Auteur", sources="utilisateur", processor='Formate_utilisateur')
 
         class Meta:
             structure_template = MyDatatable.structure_template
-            columns = ['check', 'idmail', 'date_creation', 'objet', 'nbre_destinataires']
+            columns = ['check', 'idmail', 'date_creation', 'objet', 'nbre_destinataires', "utilisateur"]
             ordering = ['date_creation']
             processors = {
                 'date_creation': helpers.format_date('%d/%m/%Y %H:%M'),
             }
 
+        def Formate_utilisateur(self, instance, **kwargs):
+            if instance.utilisateur:
+                return instance.utilisateur.get_full_name() or instance.utilisateur.get_short_name() or instance.utilisateur
+            return ""
+
 
 class Supprimer(Page, crud.Supprimer):
     pass
+
 
 class Supprimer_plusieurs(Page, crud.Supprimer_plusieurs):
     pass
--- noethysweb/outils/views/sms.py
+++ noethysweb/outils/views/sms.py
@@ -28,7 +28,7 @@ class Liste(Page, crud.Liste):
     model = SMS
 
     def get_queryset(self):
-        return SMS.objects.filter(self.Get_filtres("Q")).annotate(nbre_destinataires=Count("destinataires"))
+        return SMS.objects.select_related("utilisateur").filter(self.Get_filtres("Q")).annotate(nbre_destinataires=Count("destinataires"))
 
     def get_context_data(self, **kwargs):
         context = super(Liste, self).get_context_data(**kwargs)
@@ -40,22 +40,28 @@ class Liste(Page, crud.Liste):
 
     class datatable_class(MyDatatable):
         filtres = ['idsms', 'objet']
-
         check = columns.CheckBoxSelectColumn(label="")
         actions = columns.TextColumn("Actions", sources=None, processor='Get_actions_standard')
         nbre_destinataires = columns.TextColumn("Destinataires", sources=['nbre_destinataires'])
+        utilisateur = columns.TextColumn("Auteur", sources="utilisateur", processor='Formate_utilisateur')
 
         class Meta:
             structure_template = MyDatatable.structure_template
-            columns = ['check', 'idsms', 'date_creation', 'objet', 'nbre_destinataires']
+            columns = ['check', 'idsms', 'date_creation', 'objet', 'nbre_destinataires', "utilisateur"]
             ordering = ['date_creation']
             processors = {
                 'date_creation': helpers.format_date('%d/%m/%Y %H:%M'),
             }
 
+        def Formate_utilisateur(self, instance, **kwargs):
+            if instance.utilisateur:
+                return instance.utilisateur.get_full_name() or instance.utilisateur.get_short_name() or instance.utilisateur
+            return ""
+
 
 class Supprimer(Page, crud.Supprimer):
     pass
+
 
 class Supprimer_plusieurs(Page, crud.Supprimer_plusieurs):
     pass
