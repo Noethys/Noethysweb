d939b7de4886a7ea417a759fbe915cf23c7aebe4
Noethys <Noethys@users.noreply.github.com>
2022-03-24 22:49:24 +0100

    Fix typo in liste destinataires emails familles

--- noethysweb/outils/views/editeur_emails_familles.py
+++ noethysweb/outils/views/editeur_emails_familles.py
@@ -32,7 +32,7 @@ class Liste(Page_destinataires, crud.Liste):
     class datatable_class(MyDatatable):
         filtres = ["fpresent:pk", "fscolarise:pk", "idfamille", "nom", "rue_resid", "cp_resid", "ville_resid", "mail", "caisse__nom"]
         check = columns.CheckBoxSelectColumn(label="")
-        mail = columns.TextColumn("Rue", processor='Get_mail')
+        mail = columns.TextColumn("Email", processor='Get_mail')
         rue_resid = columns.TextColumn("Rue", processor='Get_rue_resid')
         cp_resid = columns.TextColumn("CP", processor='Get_cp_resid')
         ville_resid = columns.TextColumn("Ville",  sources=None, processor='Get_ville_resid')
