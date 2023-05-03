eb6eda640d6d0c052dbc83d65fc5538620ce92ed
Noethys <Noethys@users.noreply.github.com>
2023-05-03 08:44:37 +0200

    Fix blocage des places disponibles si attente

--- noethysweb/consommations/views/grille.py
+++ noethysweb/consommations/views/grille.py
@@ -235,7 +235,7 @@ def Get_generic_data(data={}):
     data['dict_unites_remplissage_json'] = mark_safe(json.dumps(dict_unites_remplissage_json))
 
     # Importation des places prises
-    liste_places = Consommation.objects.values('date', 'unite', 'groupe', 'quantite', 'evenement').annotate(nbre=Count('pk')).filter(data["conditions_periodes"] & Q(activite=data['selection_activite']) & Q(etat__in=("reservation", "present"))).exclude(inscription__in=data["liste_inscriptions"])
+    liste_places = Consommation.objects.values('date', 'unite', 'groupe', 'quantite', 'evenement').annotate(nbre=Count('pk')).filter(data["conditions_periodes"] & Q(activite=data['selection_activite']) & Q(etat__in=("reservation", "present", "attente"))).exclude(inscription__in=data["liste_inscriptions"])
     dict_places = {}
     for p in liste_places:
         key = "%s_%d_%d" % (p["date"], p["unite"], p["groupe"])
