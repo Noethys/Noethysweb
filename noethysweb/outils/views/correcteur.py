078e3145e06ad9c77f496915d392f1c64b732ad6
Noethys <Noethys@users.noreply.github.com>
2023-05-14 13:18:43 +0200

    Correction du correcteur

--- noethysweb/outils/views/correcteur.py
+++ noethysweb/outils/views/correcteur.py
@@ -10,6 +10,7 @@ from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.db.models.functions import Concat
 from django.db.models import Count, F, CharField, Value, Q, Min, Max
+from django.conf import settings
 from core.views.base import CustomView
 from core.models import Consommation, Prestation, Ventilation
 from outils.forms.correcteur import Formulaire
@@ -46,9 +47,14 @@ class Anomalie():
 def Get_anomalies():
     data = {}
 
+    # Recherche de la date de rétroaction
+    date_retroaction = datetime.date.today() - datetime.timedelta(days=settings.CORRECTEUR_JOURS_RETROACTION)
+
     # Recherche les doublons dans les consommations (pour un individu sur une même date avec la même unité)
     qs = Consommation.objects.select_related("individu", "inscription").annotate(doublon_id=Concat(F("individu_id"), Value("_"), F("inscription_id"), Value("_"), F("unite_id"), Value("_"), F("date"), Value("_"), F("evenement_id"), output_field=CharField()))
-    doublons = qs.values("individu__nom", "individu__prenom", "individu__pk", "inscription__famille_id", "activite__pk", "date", "doublon_id").annotate(nbre_doublons=Count("doublon_id")).filter(nbre_doublons__gt=1)
+    doublons = qs.values("individu__nom", "individu__prenom", "individu__pk", "inscription__famille_id", "activite__pk", "date", "doublon_id")\
+        .annotate(nbre_doublons=Count("doublon_id"))\
+        .filter(nbre_doublons__gt=1, date__gte=date_retroaction)
     data["Doublons de consommations (%d)" % len(doublons)] = [Anomalie(
         label="%s %s : %d doublons le %s." % (d["individu__nom"], d["individu__prenom"], d["nbre_doublons"], d["date"]),
         idcategorie="doublons_consommations", idfamille=d["inscription__famille_id"], idindividu=d["individu__pk"], idactivite=d["activite__pk"], date_min=d["date"], date_max=d["date"],
@@ -57,21 +63,27 @@ def Get_anomalies():
 
     # Recherche les doublons dans les prestations (pour un individu sur une même date avec le même tarif de prestation)
     qs = Prestation.objects.select_related("individu").annotate(doublon_id=Concat(F("individu_id"), Value("_"), F("famille_id"), Value("_"), F("tarif_id"), Value("_"), F("date"), output_field=CharField()))
-    doublons = qs.values("individu__nom", "individu__prenom", "date", "individu__pk", "famille__pk", "activite__pk", "doublon_id").annotate(nbre_doublons=Count("doublon_id")).filter(nbre_doublons__gt=1, categorie="consommation")
+    doublons = qs.values("individu__nom", "individu__prenom", "date", "individu__pk", "famille__pk", "activite__pk", "doublon_id")\
+        .annotate(nbre_doublons=Count("doublon_id"))\
+        .filter(nbre_doublons__gt=1, categorie="consommation", date__gte=date_retroaction)
     data["Doublons de prestations (%d)" % len(doublons)] = [Anomalie(
         label="%s %s : %d doublons le %s." % (d["individu__nom"], d["individu__prenom"], d["nbre_doublons"], d["date"]),
         idcategorie="doublons_prestations", idfamille=d["famille__pk"], idindividu=d["individu__pk"], idactivite=d["activite__pk"], date_min=d["date"], date_max=d["date"])
         for d in doublons]
 
     # Recherche les consommations gratuites
-    resultats = Consommation.objects.values("activite__nom").filter(prestation_id=None, etat__in=("reservation", "present", "absenti")).annotate(nbre=Count("pk"), min_date=Min("date"), max_date=Max("date"))
+    resultats = Consommation.objects.values("activite__nom").filter(prestation_id=None, etat__in=("reservation", "present", "absenti"), date__gte=date_retroaction)\
+        .annotate(nbre=Count("pk"), min_date=Min("date"), max_date=Max("date"))
     data["Consommations gratuites (%d)" % len(resultats)] = [Anomalie(
         label="%s : %s consommations (Du %s au %s)." % (r["activite__nom"], r["nbre"], r["min_date"].strftime("%d/%m/%Y"), r["max_date"].strftime("%d/%m/%Y")),
         idcategorie="conso_gratuites", corrigeable=False)
         for r in resultats]
 
     # Recherche les prestations sans consommations associées
-    resultats = Prestation.objects.select_related("individu").values("individu__nom", "individu__prenom", "individu__pk", "famille__pk", "activite__pk", "label").filter(consommation__isnull=True, tarif__isnull=False, categorie="consommation", forfait_date_debut__isnull=True).annotate(nbre=Count("pk"), min_date=Min("date"), max_date=Max("date")).order_by("individu__nom", "individu__prenom")
+    resultats = Prestation.objects.select_related("individu").values("individu__nom", "individu__prenom", "individu__pk", "famille__pk", "activite__pk", "label")\
+        .filter(consommation__isnull=True, tarif__isnull=False, tarif__type="JOURN", tarif_ligne__isnull=False, categorie="consommation", forfait_date_debut__isnull=True, date__gte=date_retroaction)\
+        .annotate(nbre=Count("pk"), min_date=Min("date"), max_date=Max("date"))\
+        .order_by("individu__nom", "individu__prenom")
     data["Prestations sans consommations associées (%d)" % len(resultats)] = [Anomalie(
         label="%s %s : %s %s (Du %s au %s)." % (r["individu__nom"], r["individu__prenom"], r["nbre"], r["label"], r["min_date"].strftime("%d/%m/%Y"), r["max_date"].strftime("%d/%m/%Y")),
         idcategorie="presta_sans_conso", idfamille=r["famille__pk"], idindividu=r["individu__pk"], idactivite=r["activite__pk"], date_min=r["min_date"], date_max=r["max_date"])
@@ -81,6 +93,9 @@ def Get_anomalies():
 
 
 def Corrige_anomalies(request=None, anomalies=[]):
+    # Recherche de la date de rétroaction
+    date_retroaction = datetime.date.today() - datetime.timedelta(days=settings.CORRECTEUR_JOURS_RETROACTION)
+
     # Importation de la grille virtuelle
     from consommations.utils.utils_grille_virtuelle import Grille_virtuelle
 
@@ -98,7 +113,8 @@ def Corrige_anomalies(request=None, anomalies=[]):
         # Suppression de la prestation fantôme
         if anomalie.idcategorie == "presta_sans_conso":
             conditions = Q(famille_id=anomalie.idfamille, individu_id=anomalie.idindividu, activite_id=anomalie.idactivite, date__gte=anomalie.date_min, date__lte=anomalie.date_max)
-            for prestation in Prestation.objects.filter(conditions, facture__isnull=True, consommation__isnull=True, tarif__isnull=False, categorie="consommation", forfait_date_debut__isnull=True):
+            for prestation in Prestation.objects.filter(conditions, facture__isnull=True, consommation__isnull=True, tarif__isnull=False, tarif__type="JOURN", tarif_ligne__isnull=False,
+                                                        categorie="consommation", forfait_date_debut__isnull=True, date__gte=date_retroaction):
                 Ventilation.objects.filter(prestation=prestation).delete()
                 prestation.delete()
 
@@ -124,7 +140,7 @@ class View(CustomView, TemplateView):
         context = super(View, self).get_context_data(**kwargs)
         context['page_titre'] = "Correcteur d'anomalies"
         context['box_titre'] = ""
-        context['box_introduction'] = "Voici la liste des anomalies détectées dans les données."
+        context['box_introduction'] = "Voici la liste des anomalies détectées dans les données dans les %d derniers jours." % settings.CORRECTEUR_JOURS_RETROACTION
         context['form'] = Formulaire(request=self.request, anomalies=Get_anomalies())
         return context
 
