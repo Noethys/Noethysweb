cb3b77bf758a378e3835713a50dbb83c1d6b669b
faouzia.takaya <faouzia.takaya@recia.fr>
2025-03-13 01:29:25 +0100

    ADD Portail famille: configuration de la page de login , accueil et fiche de profil

--- noethysweb/portail/views/change_password.py
+++ noethysweb/portail/views/change_password.py
@@ -10,12 +10,22 @@ from django.contrib.auth import update_session_auth_hash
 from portail.forms.change_password import MyPasswordChangeForm
 from portail.views.login import ClassCommuneLogin
 from portail.utils import utils_secquest
+from core.models import Famille
 
 
 class MyPasswordChangeView(ClassCommuneLogin, auth_views.PasswordChangeView):
     form_class = MyPasswordChangeForm
     success_url = reverse_lazy('password_change_done')
     template_name = "portail/password_change_form.html"
+
+    def get_form_kwargs(self):
+        # Appel à la méthode de base pour obtenir les arguments initiaux
+        kwargs = super().get_form_kwargs()
+
+        # Ajout de l'utilisateur actuel aux arguments du formulaire
+        kwargs['user'] = self.request.user
+
+        return kwargs
 
     def get_context_data(self, **kwargs):
         context = super(MyPasswordChangeView, self).get_context_data(**kwargs)
@@ -29,27 +39,44 @@ class MyPasswordChangeView(ClassCommuneLogin, auth_views.PasswordChangeView):
         return context
 
     def form_valid(self, form):
-        # Vérification de la secquest
+        # Vérification de la secquest (applicable pour la famille et individu)
         if "secquest" in form.cleaned_data:
-            if not utils_secquest.Check_secquest(famille=self.request.user.famille, reponse=form.cleaned_data["secquest"]):
-                form.add_error(None, _("La réponse à la question est erronée"))
-                return self.render_to_response(self.get_context_data(form=form))
+            if hasattr(self.request.user, 'famille'):
+                # Si l'utilisateur est associé à une famille, vérifier la sécurité pour la famille
+                if not utils_secquest.Check_secquest(famille=self.request.user.famille,
+                                                     reponse=form.cleaned_data["secquest"]):
+                    form.add_error(None, _("La réponse à la question est erronée"))
+                    return self.render_to_response(self.get_context_data(form=form))
+            elif hasattr(self.request.user, 'individu'):
+                # Si l'utilisateur est un individu, vous pouvez ajouter une logique de vérification ici si nécessaire
+                famille = Famille.objects.filter(titulaire_helios_id=self.request.user.individu).first()
+                if famille and not utils_secquest.Check_secquest(famille=famille,
+                                                                 reponse=form.cleaned_data["secquest"]):
+                    form.add_error(None, _("La réponse à la question est erronée"))
+                    return self.render_to_response(self.get_context_data(form=form))
 
+        # Sauvegarder le formulaire
         form.save()
         utilisateur = form.user
 
         # Enregistrement du nouveau mot de passe
         update_session_auth_hash(self.request, utilisateur)
-
-        # Enregistre le champ force_reset_password de l'utilisateur
         utilisateur.force_reset_password = False
         utilisateur.date_expiration_mdp = None
         utilisateur.save()
-        utilisateur.famille.internet_mdp = "*****"
-        utilisateur.famille.save()
+
+        # Vérification si l'utilisateur est associé à une famille ou à un individu
+        if hasattr(utilisateur, 'famille'):
+            # Mise à jour des informations de la famille
+            utilisateur.famille.internet_mdp = "*****"
+            utilisateur.famille.save()
+
+        elif hasattr(utilisateur, 'individu'):
+            # Mise à jour des informations de l'individu
+            utilisateur.individu.internet_mdp = "*****"
+            utilisateur.individu.save()
 
         return super().form_valid(form)
-
 
 class MyPasswordChangeDoneView(ClassCommuneLogin, auth_views.PasswordChangeDoneView):
     template_name = "portail/password_change_done.html"
