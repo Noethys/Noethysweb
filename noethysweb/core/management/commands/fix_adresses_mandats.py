58731929832f04fdcc6933c3aea014a9598ba959
Noethys <Noethys@users.noreply.github.com>
2025-10-13 22:23:19 +0200

    Simplification fix adresse mandats

--- noethysweb/core/management/commands/fix_adresses_mandats.py
+++ noethysweb/core/management/commands/fix_adresses_mandats.py
@@ -12,13 +12,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         from core.utils import utils_adresse
-        mandats = Mandat.objects.filter(individu__isnull=True)
+        mandats = Mandat.objects.filter(individu__isnull=True, individu_rue__isnull=False, individu_numero__isnull=True)
         for mandat in mandats:
-            if mandat.individu_rue and not mandat.individu_numero:
-                detail = utils_adresse.Extraire_numero_rue(rue=mandat.individu_rue)
-                if detail:
-                    mandat.individu_numero = detail[0]
-                    mandat.individu_rue = detail[1]
-                    mandat.save()
+            detail = utils_adresse.Extraire_numero_rue(rue=mandat.individu_rue)
+            if detail:
+                mandat.individu_numero = detail[0]
+                mandat.individu_rue = detail[1]
+                mandat.save()
 
         self.stdout.write(self.style.SUCCESS("Correction de la structuration des adresses des mandats OK"))
