78aea492448f9de1d98ec60aa51246e89a883445
Noethys <Noethys@users.noreply.github.com>
2025-11-06 17:35:15 +0100

    Fix version xsd dans export prélèvements

--- noethysweb/facturation/utils/utils_export_prelevements.py
+++ noethysweb/facturation/utils/utils_export_prelevements.py
@@ -106,7 +106,7 @@ class Exporter():
         # Génération du document XML
         racine = doc.createElement("Document")
         racine.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
-        racine.setAttribute("xmlns", "urn:iso:std:iso:20022:tech:xsd:pain.008.001.%s" % "08" if self.lot.modele.version_sepa == "2019" else "02")
+        racine.setAttribute("xmlns", "urn:iso:std:iso:20022:tech:xsd:pain.008.001.%s" % ("08" if self.lot.modele.version_sepa == "2019" else "02"))
         doc.appendChild(racine)
 
         # CstmrDrctDbtInitn
