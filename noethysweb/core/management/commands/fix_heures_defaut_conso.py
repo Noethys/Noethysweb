d207ece1e9061452b61396f99b0f10ac2c7fbbdc
Noethys <Noethys@users.noreply.github.com>
2026-03-04 18:20:27 +0100

    Amélioration command fix heures conso

--- noethysweb/core/management/commands/fix_heures_defaut_conso.py
+++ noethysweb/core/management/commands/fix_heures_defaut_conso.py
@@ -3,20 +3,36 @@
 #  Noethysweb, application de gestion multi-activités.
 #  Distribué sous licence GNU GPL.
 
+import datetime
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 from core.models import Consommation
 
 
 class Command(BaseCommand):
-    help = "Correction des heures par défaut des consommations de type unitaire "
+    help = "Correction des heures par défaut des consommations de type unitaire. Options : date_debut, date_fin, idactivite."
+
+    def add_arguments(self, parser):
+        parser.add_argument("--date_debut", nargs="?", type=lambda s: datetime.datetime.strptime(s, "%d/%m/%Y").date(), help="Date au format FR")
+        parser.add_argument("--date_fin", nargs="?", type=lambda s: datetime.datetime.strptime(s, "%d/%m/%Y").date(), help="Date au format FR")
+        parser.add_argument("--idactivite", nargs="?", type=int, help="ID de l'activité", default=0)
 
     def handle(self, *args, **kwargs):
         """ Affecte les heures par défaut aux consommations de type Unitaire """
         nbre_corrections = 0
-        for conso in Consommation.objects.select_related("unite").filter(heure_debut__isnull=True, heure_fin__isnull=True):
+        conditions = (Q(heure_debut__isnull=True) | Q(heure_fin__isnull=True))
+        if kwargs["idactivite"]:
+            conditions &= Q(activite_id=kwargs["idactivite"])
+        if kwargs["date_debut"]:
+            conditions &= Q(date__gte=kwargs["date_debut"])
+        if kwargs["date_fin"]:
+            conditions &= Q(date__lte=kwargs["date_fin"])
+        for conso in Consommation.objects.select_related("unite").filter(conditions):
             if conso.unite.type == "Unitaire" and conso.unite.heure_debut and conso.unite.heure_fin:
-                conso.heure_debut = conso.unite.heure_debut
-                conso.heure_fin = conso.unite.heure_fin
+                if not conso.heure_debut:
+                    conso.heure_debut = conso.unite.heure_debut
+                if not conso.heure_fin:
+                    conso.heure_fin = conso.unite.heure_fin
                 conso.save()
                 nbre_corrections += 1
 
